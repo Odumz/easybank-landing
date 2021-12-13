f40ae76e5b8e84002e7c942924ff5291a427d03c
Odumz <odumz20@gmail.com>
2021-12-13 14:16:22 +0100

    code refactor

---
+++ src/components/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <div class="title">
+      <h1>Why choose Easybank?</h1>
+      <p>
+        We leverage Open Banking to turn your bank account into your financial
+        hub. Control your finances like never before.
+      </p>
+    </div>
+    <div class="body">
+      <service />
+    </div>
+  </div>
+</template>
